ea4763230694c74db08b8acce9d78c09f375fb6b
Morten Daniel Fornes <mdfornes@gmail.com>
2020-06-12 22:39:48 +0700

    import System.Drawing.Common.dll

--- sync.sh
+++ sync.sh
@@ -5,3 +5,4 @@ LIB="/Library/Frameworks/Xamarin.Mac.framework/Versions/$VERSION/lib"
 cp "$LIB/libxammac.dylib" .
 cp "$LIB/reference/full/OpenTK.dll" .
 cp "$LIB/reference/full/Xamarin.Mac.dll" .
+cp "$LIB/mono/4.5/Facades/System.Drawing.Common.dll" .
