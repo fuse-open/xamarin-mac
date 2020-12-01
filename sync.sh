fde58f01cfaa7b3a82cb3fd0dbc0c1c7ec903305
Morten Daniel Fornes <mdfornes@gmail.com>
2020-12-01 22:27:43 +0700

    upgrade to v7.0.0.15

--- sync.sh
+++ sync.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-VERSION="6.16.0.13"
+VERSION="7.0.0.15"
 LIB="/Library/Frameworks/Xamarin.Mac.framework/Versions/$VERSION/lib"
 
-cp "$LIB/libxammac.dylib" .
+cp "$LIB/../SDKs/Xamarin.macOS.sdk/lib/libxammac.dylib" .
 cp "$LIB/reference/full/OpenTK.dll" .
 cp "$LIB/reference/full/Xamarin.Mac.dll" .
 cp "$LIB/mono/4.5/Facades/System.Drawing.Common.dll" .
